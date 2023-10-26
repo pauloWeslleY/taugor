b4a6f9f76ed5c041f4485abfb8b7be7141fe31bf
ribeiro-jr <ribeirojr1522@gmail.com>
2023-10-26 19:22:46 -0300

    chore: Adicionando configuracoes globais

--- src/config/icons/index.tsx
+++ src/config/icons/index.tsx
@@ -1,8 +1,10 @@
 import { FaEdit, FaHome, FaInfoCircle, FaLightbulb } from 'react-icons/fa';
+import { BiLoaderCircle } from 'react-icons/bi';
 
 export const icons = {
   home: <FaHome />,
   info: <FaInfoCircle />,
   edit: <FaEdit />,
-  lightBulb: <FaLightbulb />
+  lightBulb: <FaLightbulb />,
+  loading: <BiLoaderCircle />
 }
