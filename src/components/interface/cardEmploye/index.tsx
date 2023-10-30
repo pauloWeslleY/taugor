674d20dd4e2d2c5f01d3cc37646c21ead2ff3f46
PabloAllves <ribeirojr1522@gmail.com>
2023-10-30 19:21:52 -0300

    feat: Criando componentes com chakra ui

--- src/components/interface/cardEmploye/index.tsx
+++ src/components/interface/cardEmploye/index.tsx
@@ -1,6 +1,4 @@
-import styles from "./cardEmploye.module.css";
 import { icons } from "../../../config/icons";
-import { Link } from "react-router-dom";
 
 import AvatarGirl from "../../../assets/avatar-feminino.png";
 import AvatarBoy from "../../../assets/avatar-masculino.png";
@@ -11,6 +9,20 @@ import {
 } from "../../../contexts/employerContext";
 import { handleRenderRoleOrSector } from "../../../utils";
 
+import { ChakraProvider } from "@chakra-ui/react";
+
+import {
+  Badge,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 interface CardEmployeProps {
   employe: DataEmploye;
 }
@@ -19,66 +31,96 @@ export function CardEmploye({ employe }: CardEmployeProps) {
   const { listRoles, listSectors } = useContext(EmployerContext);
 
   return (
-    <section className={styles.cardEmploye}>
-      {employe?.profileUrl ? (
-        <img src={employe.profileUrl} alt="Foto de Perfil" />
-      ) : employe?.sex === "masculine" ? (
-        <img src={AvatarBoy} alt="Foto de Perfil" />
-      ) : (
-        <img src={AvatarGirl} alt="Foto de Perfil" />
-      )}
-      <section className={styles.infoCardEmploye}>
-        <section
-          className={
-            employe?.status === "active"
-              ? styles.areaHeaderCardEmployeActive
-              : styles.areaHeaderCardEmployeInactive
-          }
-        >
-          <span>{icons.circle}</span>
-          <p>{employe?.status === "active" ? "Ativo" : "Inativo"}</p>
-        </section>
-        <section className={styles.cardInfoGrid}>
-          <p>
-            <strong>Nome:</strong>
-            <span>{employe?.name}</span>
-          </p>
-          <p>
-            <strong>email:</strong>
-            <span>{employe?.email}</span>
-          </p>
-          <p>
-            <strong>CPF:</strong>
-            <span>{employe?.cpf}</span>
-          </p>
-          <p>
-            <strong>Tel:</strong>
-            <span>{employe?.tel}</span>
-          </p>
-          <p>
-            <strong>Setor:</strong>
-            <span>
-              {handleRenderRoleOrSector({
-                id: employe?.sector,
-                list: listSectors,
-              })}
-            </span>
-          </p>
-          <p>
-            <strong>Cargo:</strong>
-            <span>
+    <ChakraProvider>
+      <Stack
+        borderWidth="1px"
+        borderRadius="lg"
+        w={{ sm: "100%", md: "400px" }}
+        height={{ sm: "250px" }}
+        direction={{ base: "column", md: "row" }}
+        bg={useColorModeValue("white", "gray.900")}
+        boxShadow={"2xl"}
+        padding={2}
+      >
+        <Flex flex={1} bg="blue.200">
+          {employe?.profileUrl ? (
+            <Image
+              objectFit="cover"
+              boxSize="100%"
+              src={employe?.profileUrl}
+              alt="Foto de perfil"
+              borderRadius={"0.2rem"}
+            />
+          ) : (
+            <Image
+              objectFit="cover"
+              boxSize="100%"
+              src={employe?.sex === "masculine" ? AvatarBoy : AvatarGirl}
+              alt="Foto de perfil"
+              borderRadius={"0.2rem"}
+            />
+          )}
+        </Flex>
+
+        <Stack flex={1} flexDirection="column" alignItems="center">
+          <Flex
+            bg={employe?.status === "active" ? "green.400" : "red.400"}
+            alignSelf={"flex-end"}
+            px={2}
+            py={1}
+            borderRadius={"0.3rem"}
+            color={"white"}
+            marginBottom={2}
+          >
+            {employe?.status === "active" ? "ATIVO" : "INATIVO"}
+          </Flex>
+          <Heading fontSize={"2xl"} fontFamily={"body"}>
+            {employe?.name}
+          </Heading>
+          <Text fontWeight={500} color={"gray.500"} size="sm">
+            {employe?.email}
+          </Text>
+          <Stack align={"center"} justify={"center"} direction={"row"} mt={6}>
+            <Badge
+              px={2}
+              py={1}
+              bg={useColorModeValue("gray.50", "gray.800")}
+              fontWeight={"400"}
+            >
+              #{" "}
               {handleRenderRoleOrSector({ id: employe?.role, list: listRoles })}
-            </span>
-          </p>
-          <p>
-            <strong>Admissão:</strong>
-            <span>{employe?.dateAdmission}</span>
-          </p>
-          <Link to={`/detail/${employe.id}`} className={styles.detailLink}>
-            <span>{icons.plus}</span> Detalhes
-          </Link>
-        </section>
-      </section>
-    </section>
+            </Badge>
+          </Stack>
+
+          <Stack
+            width={"100%"}
+            direction={"row"}
+            padding={1}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <Button
+              as={Link}
+              to={`/detail/${employe?.id}`}
+              flex={1}
+              bg={"blue.400"}
+              color={"white"}
+              gap={2}
+              boxShadow={
+                "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+              }
+              _hover={{
+                bg: "blue.500",
+              }}
+              _focus={{
+                bg: "blue.500",
+              }}
+            >
+              {icons.plus} Detalhes
+            </Button>
+          </Stack>
+        </Stack>
+      </Stack>
+    </ChakraProvider>
   );
 }
--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,9 +1,52 @@
-
+import {
+  Box,
+  Heading,
+  Text,
+  Button,
+  ChakraProvider,
+  Flex,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <main>
-      pagina não encontrada
-    </main>
-  )
+    <ChakraProvider>
+      <Flex
+        textAlign="center"
+        flexDir={"column"}
+        justify={"center"}
+        py={10}
+        px={6}
+        h={"full"}
+      >
+        <Heading
+          display="inline-block"
+          as="h2"
+          size="2xl"
+          bgGradient="linear(to-r, blue.200, blue.900)"
+          backgroundClip="text"
+        >
+          404
+        </Heading>
+        <Text fontSize="18px" mt={3} mb={2}>
+          Página não encontrada
+        </Text>
+        <Text color={"gray.500"} mb={6}>
+          Clique no botão e navegue para a página inicial
+        </Text>
+
+        <Flex align={"center"} justify={"center"}>
+          <Button
+            colorScheme="blue"
+            bgGradient="linear(to-r, blue.400, blue.500, blue.600)"
+            color="white"
+            as={Link}
+            to={"/home"}
+          >
+            Página inicial
+          </Button>
+        </Flex>
+      </Flex>
+    </ChakraProvider>
+  );
 }
